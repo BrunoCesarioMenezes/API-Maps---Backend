9cfc28edc1425c3a858bc83231380135318a0240
BrunoCesarioMenezes <brunoagjf@gmail.com>
2025-10-07 15:19:12 -0300

    corrigindo vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+
+const APP_URL = process.env.VITE_APP_URL || 'http://localhost:8000';
 
 export default defineConfig({
     plugins: [
@@ -12,9 +15,7 @@ export default defineConfig({
         }),
         react(),
         tailwindcss(),
-        wayfinder({
-            generate: false, // evita que rode artisan no build do Railway
-        }),
+        wayfinder({ generate: false }),
     ],
-    base: import.meta.env.VITE_APP_URL + '/build/',
+    base: `${APP_URL}/build/`,
 });
